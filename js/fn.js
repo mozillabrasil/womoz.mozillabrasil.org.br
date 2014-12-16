faf09598df7feb254f41bea386b418f2cab5ff44
Cynthia Zanoni <cynthiazanoni@gmail.com>
2014-12-16 21:34:10 -0200

    [+] Initial Structure and style made with SASS <3

---
+++ js/fn.js
@@ -0,0 +1,70 @@
+//========================
+//PRELOADER
+//========================
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow');
+    // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow':'visible'});
+})
+
+//========================
+//CUSTOM SCROLLBAR
+//========================
+$("html").niceScroll({
+    mousescrollstep: 70,
+    cursorcolor: "#ea9312",
+    cursorwidth: "5px",
+    cursorborderradius: "10px",
+    cursorborder: "none",
+});
+
+//========================
+//SMOOTHSCROLL
+//========================
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//========================
+//NAVBAR
+//========================
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 40) {
+                $('.navbar')
+                .removeClass('animated fadeOutUp')
+                .addClass('animated fadeInDown')
+                .fadeIn();
+                
+            } else {
+                $('.navbar')
+                .removeClass('animated fadeInDown')
+                .addClass('animated fadeOutUp')
+                .fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
