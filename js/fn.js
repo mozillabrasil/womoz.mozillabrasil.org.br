354210ea27c12d8060904842ba90614e6b83cf12
Guilherme Berghauser <tortoyoyo@gmail.com>
2014-12-17 01:49:01 -0200

    Adicionado a leitura de rss nos eventos - Firefox não funciona

--- js/fn.js
+++ js/fn.js
@@ -1,70 +1,107 @@
-//========================
-//PRELOADER
-//========================
-$(window).load(function() { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow');
-    // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow':'visible'});
-})
+(function($) {
+    $(document).ready(function() {
 
-//========================
-//CUSTOM SCROLLBAR
-//========================
-$("html").niceScroll({
-    mousescrollstep: 70,
-    cursorcolor: "#ea9312",
-    cursorwidth: "5px",
-    cursorborderradius: "10px",
-    cursorborder: "none",
-});
+        $('#shotsByPlayerId').feeds({
+            feeds: {
+                evento: 'http://tortoyoyo.tumblr.com/tagged/evento/rss'
+            },
+            ssl: 'false',
+            entryTemplate: function(entry) {
+                var template = '';
 
-//========================
-//SMOOTHSCROLL
-//========================
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+                if (entry.source == 'evento') {
 
-//========================
-//NAVBAR
-//========================
-(function ($) {
-  $(document).ready(function(){
+                    var splitvalores = entry.content.split('</p>');
+                    var img = splitvalores[0] + '</p>';
+                    var conteudo = splitvalores[1] + '</p>';
 
-    // hide .navbar first
-    $(".navbar").hide();
+                    template += '<li>'
+                    template += '<a href="#">'
+                    template += img
+                    template += '<h3><a href="#"><!=title!></a></h3>'
+                    template += '<div class="likecount">'
+                    template += '<a href="#"><span class="icon-heart"></span> 71</a>'
+                    template += '</div>'
+                    template += '<div class="commentcount">'
+                    template += '<a href="#"><span class="icon-bubbles"></span> 15</a>'
+                    template += '</div>'
+                    template += '</li>'
 
-    // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
+                  
+                } else if (entry.source == 'pessoa') {
+                    // Just the content for twitter entries
+                    template = '<div><!=content!></div>';
+                }
 
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 40) {
-                $('.navbar')
-                .removeClass('animated fadeOutUp')
-                .addClass('animated fadeInDown')
-                .fadeIn();
-                
-            } else {
-                $('.navbar')
-                .removeClass('animated fadeInDown')
-                .addClass('animated fadeOutUp')
-                .fadeOut();
+                // Render the template
+                return this.tmpl(template, entry);
             }
         });
-    });
 
-});
-  }(jQuery));
+        //========================
+        //PRELOADER
+        //========================
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow');
+        // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+
+
+        //========================
+        //CUSTOM SCROLLBAR
+        //========================
+        $("html").niceScroll({
+            mousescrollstep: 70,
+            cursorcolor: "#ea9312",
+            cursorwidth: "5px",
+            cursorborderradius: "10px",
+            cursorborder: "none",
+        });
+
+        //========================
+        //SMOOTHSCROLL
+        //========================
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+
+        //========================
+        //NAVBAR
+        //========================
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function() {
+            $(window).scroll(function() {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 40) {
+                    $('.navbar')
+                        .removeClass('animated fadeOutUp')
+                        .addClass('animated fadeInDown')
+                        .fadeIn();
+
+                } else {
+                    $('.navbar')
+                        .removeClass('animated fadeInDown')
+                        .addClass('animated fadeOutUp')
+                        .fadeOut();
+                }
+            });
+        });
+
+    });
+}(jQuery));
