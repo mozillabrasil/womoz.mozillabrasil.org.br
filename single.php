e59257ed7797ad78d88f8291abd80676904cd594
cristianounix <cristianounix@gmail.com>
2015-09-13 01:41:20 -0300

    Botoes para compartilhamento

--- single.php
+++ single.php
@@ -16,6 +16,31 @@
 				<li class="comment"><a href="<?php comments_link(); ?>"><span class="icon-bubbles"></span> <?php comments_number( __( 'Comentar', 'womoz' ), __( '1 Comentário', 'womoz' ), __( '% Comentários', 'womoz' ) ); ?></a></li>
 			</ul>
 			<?php the_content(); ?>
+
+			<div class="social_links">
+
+		      <a href="https://twitter.com/share" class="twitter-share-button" data-via="womoz.mozillabrasil" data-related="mozillabrasil" data-hashtags="womoz">Tweet</a> 
+		      <script>
+		      		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+		      		if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+		      		fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+		      </script>
+
+		      <div class="fb-share-button" data-href="<?php echo get_permalink();?>" data-layout="button_count"></div>
+		      <div id="fb-root"></div>
+		      <script>
+		      	(function(d, s, id) {
+		        var js, fjs = d.getElementsByTagName(s)[0];
+		        if (d.getElementById(id)) return;
+		        js = d.createElement(s); js.id = id;
+		        js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.4&appId={APPID-DA-WOMOZ}";
+		        fjs.parentNode.insertBefore(js, fjs);
+		      }(document, 'script', 'facebook-jssdk'));
+		      </script>
+
+		    </div>
+		    </div>
+			</div>
 		</div>
 
 		<?php comments_template( '', true ); ?>
